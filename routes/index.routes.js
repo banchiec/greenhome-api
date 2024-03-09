6a44ba185945facf54a543be56fd4d04639dc975
banchiec <icaro_19@hotmail.com>
2024-03-09 01:13:59 +0100

    festure/refactor-routes comments-routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const authRoutes = require('./auth.routes/index.auth.routes')
+const commentRoutes = require('./comments.routes/commentsRoutes')
 
 router.use('/auth', authRoutes)
+router.use('/comments', commentRoutes)
 
 module.exports = router
